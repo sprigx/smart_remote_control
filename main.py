c4a9cf7b460dcb4c8e852807316856324a1ce934
sprig <springboardyr@gmail.com>
2021-09-03 17:10:06 +0900

    webserverとcontrollerに分けた

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 import subprocess
+import textwrap
 
 app = FastAPI()
 
-@app.get("/control")
-async def control(target, command):
+# リクエストbodyを定義
+class Request(BaseModel):
+    target: str
+    command: str
+
+@app.post("/control")
+def control(request: Request):
     try:
-        res = subprocess.check_output(f'python3 irrp.py -p -g17 -f codes {target}:{command}', shell=True)
+        cmd = ('python3 irrp.py -p -g17 -f codes '
+              f'{request.target}:{request.command}')
+        res = subprocess.check_output(cmd, shell=True)
+        print(cmd)
         return {"detail": "ok"}
     except:
         return {"detail": "failed"}
