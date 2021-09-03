317166752e85f3dde903f753c3bfb108ed1eaaa3
sprig <springboardyr@gmail.com>
2021-09-03 20:51:23 +0900

    add_test

--- controller/main.py
+++ controller/main.py
@@ -22,6 +22,9 @@ def control(request: Request):
     except:
         return {"detail": "failed"}
 
+@app.get("/test")
+def test():
+    return {"detail": "ok"}
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=8000)
