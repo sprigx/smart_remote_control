2721c81d1777248dbae14e02af02ce5e39a54f47
sprig <springboardyr@gmail.com>
2021-09-04 22:19:54 +0900

    クラス化したirrpをAPIに移植

--- controller/main.py
+++ controller/main.py
@@ -5,6 +5,7 @@ import textwrap
 import uvicorn
 import os
 from test import Test
+from remote_controller import RemoteController
 
 def build_filepath(relative_path):
     return os.path.join(os.path.dirname(__file__), relative_path)
@@ -19,20 +20,19 @@ class Request(BaseModel):
 @app.post("/control")
 def control(request: Request):
     try:
-        cmd = (f"python3 {build_filepath('irrp.py')} -p -g17 -f {build_filepath('codes')} "
-               f'{request.target}:{request.command}')
-        res = subprocess.check_output(cmd, shell=True)
-        print(cmd)
+        # cmd = (f"python3 {build_filepath('irrp.py')} -p -g17 -f {build_filepath('codes')} "
+        #        f'{request.target}:{request.command}')
+        # res = subprocess.check_output(cmd, shell=True)
+        # print(cmd)
+        c.transmit(request.target, request.command)
         return {"detail": "ok"}
     except:
         return {"detail": "failed"}
 
 @app.get("/test")
 def test():
-    plus = gpio_handler.execute('+')
-    minus = gpio_handler.execute('-')
-    return {"detail": {"plus": plus, "minus": minus}}
+    return {"detail": "this is a test."}
 
 if __name__ == '__main__':
-    gpio_handler = Test()
+    c = RemoteController(17)
     uvicorn.run(app, host='0.0.0.0', port=8000)
