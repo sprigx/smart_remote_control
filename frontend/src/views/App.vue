c7edaf98047d4c45915ebb06c48c5ecc1fb53d21
sprig <springboardyr@gmail.com>
2021-09-08 23:41:51 +0900

    ボタンを増やした

--- frontend/src/views/App.vue
+++ frontend/src/views/App.vue
@@ -1,10 +1,21 @@
 <template>
 <div class="container">
-  <h1>Remote Controller</h1>
+  <h1 class="mt-4">Remote Controller</h1>
   <hr>
-  <div class="d-grid gap-2">
-    <button class="btn btn-primary" @click="transmit('dac', 'volup')">DAC vol-up</button>
-    <button class="btn btn-primary" @click="transmit('dac', 'voldown')">DAC vol-down</button>
+  <h3>DAC</h3>
+  <hr>
+  <div class="d-grid gap-2 d-md-block">
+    <button class="btn btn-warning btn-inline p-4" @click="transmit('dac', 'pwr')">Power</button>
+    <button class="btn btn-primary btn-inline p-4" @click="transmit('dac', 'volup')">Vol-up</button>
+    <button class="btn btn-primary btn-inline p-4" @click="transmit('dac', 'voldown')">Vol-down</button>
+    <button class="btn btn-success btn-inline p-4" @click="transmit('dac', 'coax')">Coax</button>
+    <button class="btn btn-success btn-inline p-4" @click="transmit('dac', 'usb')">USB</button>
+  </div>
+  <h3>Fan</h3>
+  <hr>
+  <div class="d-grid gap-2 d-md-block">
+    <button class="btn btn-warning btn-inline p-4" @click="transmit('fan', 'pwr')">Power</button>
+    <button class="btn btn-primary btn-inline p-4" @click="transmit('fan', 'vol')">Vol</button>
   </div>
 </div>
 </template>
@@ -34,6 +45,10 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+}
+
+.btn-inline {
+  margin: 10px 20px;
 }
 
 </style>
