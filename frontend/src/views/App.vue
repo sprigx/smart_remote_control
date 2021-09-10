8bf608c8ad8ce1791e6a6b27a9458ac374e4a9ae
sprig <springboardyr@gmail.com>
2021-09-10 12:33:05 +0900

    ボタン追加

--- frontend/src/views/App.vue
+++ frontend/src/views/App.vue
@@ -2,20 +2,27 @@
 <div class="container">
   <h1 class="mt-4">Remote Controller</h1>
   <hr>
-  <h3>DAC</h3>
+  <h3 class="mt-3">DAC</h3>
   <hr>
   <div class="d-grid gap-2 d-md-block">
-    <button class="btn btn-warning btn-inline p-4" @click="transmit('dac', 'pwr')">Power</button>
+    <button class="btn btn-warning btn-inline p-4" @click="transmit('dac', 'pwr')"><i class="bi bi-power"></i>Power</button>
     <button class="btn btn-primary btn-inline p-4" @click="transmit('dac', 'volup')">Vol-up</button>
     <button class="btn btn-primary btn-inline p-4" @click="transmit('dac', 'voldown')">Vol-down</button>
     <button class="btn btn-success btn-inline p-4" @click="transmit('dac', 'coax')">Coax</button>
     <button class="btn btn-success btn-inline p-4" @click="transmit('dac', 'usb')">USB</button>
+    <button class="btn btn-secondary btn-inline p-4" @click="transmit('dac', 'bt')">B/T</button>
+    <button class="btn btn-secondary btn-inline p-4" @click="transmit('dac', 'mute')">Mute</button>
   </div>
-  <h3>Fan</h3>
+  <h3 class="mt-3">Fan</h3>
   <hr>
   <div class="d-grid gap-2 d-md-block">
     <button class="btn btn-warning btn-inline p-4" @click="transmit('fan', 'pwr')">Power</button>
     <button class="btn btn-primary btn-inline p-4" @click="transmit('fan', 'vol')">Vol</button>
+  </div>
+  <h3 class="mt-3">Air Conditioner</h3>
+  <hr>
+  <div class="d-grid gap-2 d-md-block">
+    <button class="btn btn-warning btn-inline p-4" @click="transmit('ac', 'pwr')">Power</button>
   </div>
 </div>
 </template>
